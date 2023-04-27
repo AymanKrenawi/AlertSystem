938c8411a1f697c015b8478a4c46aec12307f150
Ahat <ahat.saraiah@gmail.com>
2023-04-27 18:08:54 +0300

    sort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './table/material-module';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 import { TableComponent } from './table/table.component';
+import { MatSortModule } from '@angular/material/sort';
 @NgModule({
   declarations: [
     AppComponent,
@@ -11,9 +17,19 @@ import { TableComponent } from './table/table.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatSortModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  // entryComponents: [TableComponent],
+  bootstrap: [AppComponent,TableComponent],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ]
 })
 export class AppModule { }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,29 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-//import { Element } from 'dom';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import { Component, OnInit ,ViewChild,AfterViewInit,OnChanges, SimpleChanges} from '@angular/core';
+import { MatTableDataSource, MatTable } from '@angular/material/table';
 import * as XLSX from 'xlsx';
+import {MatSort,Sort} from '@angular/material/sort';
 
+export interface columnElements {
+  carID:number;
+  licenseDate:Date;
+  testDate:Date;
+  hosDate:Date;
+  calibration:Date;
+  category:String;
+  status:String;
+  comments:String;
+}
 @Component({
   selector: 'tableComponent',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit {
-  table: any[][];
+export class TableComponent implements OnInit,OnChanges{
+  element: string;
+  dataSource: MatTableDataSource<any>;
   editMode: boolean;
- 
-  constructor() {
-    this.table = [];
-    this.editMode=false;
+  displayedColumns: string[] = ["מספר רכב", "תאריך רשיון", "תאריך טסט", "תאריך חו''ס","כיול", "קטגוריה", "סטטוס","הערות"];
+  today = new Date();
+  tableWithStatus: any[][];
+  element_data: columnElements[]|any;
+
+
+  constructor(private _liveAnnouncer: LiveAnnouncer) {
+    //this.dataSource = new MatTableDataSource<any>();
+    this.dataSource = new MatTableDataSource(this.element_data);
+
+    this.element = '';
+    this.editMode = false;
+    this.tableWithStatus = [];
   }
 
-  ngOnInit() {
+  @ViewChild('empTbSort') empTbSort = new MatSort();
+
+  ngOnChanges(changes: SimpleChanges):void {    
+    if(changes['dataSource']?.currentValue.length){
+    this.dataSource.sort = this.empTbSort;
+    }
+  }
+
+  /** Announce the change in sort state */
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+
+      this.dataSource.sort=this.empTbSort;
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  ngOnInit(): void {
     // Load data from local storage
     const savedTable = localStorage.getItem('table');
     console.log(savedTable);
     if (savedTable) {
-      this.table = JSON.parse(savedTable);
-    } else {
-      this.table = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+      this.dataSource.data = JSON.parse(savedTable);
     }
     this.editMode = false;
   }
@@ -36,58 +75,113 @@ export class TableComponent implements OnInit {
       const reader = new FileReader();
       reader.onload = () => {
         const data = new Uint8Array(reader.result as ArrayBuffer);
-        const workbook = XLSX.read(data, { type: 'array' });
+        const workbook = XLSX.read(data,{type:"array",cellDates: true});
         const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
-        this.table = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+        const date_format = 'dd.mm.yyyy'; 
+
+        // this.dataSource.data = XLSX.utils.sheet_to_json(worksheet, {
+        //   raw: false,
+        //   header: 1,
+        //   dateNF: date_format
+        // })
+
+        this.element_data = XLSX.utils.sheet_to_json(worksheet, {
+          raw: false,
+          header: 1,
+          dateNF: date_format
+        })
         this.editMode = false;
       };
       reader.readAsArrayBuffer(files[0]);
     }
   }
   
-  addRow() {
-    this.table.push(Array(this.table[0].length).fill(' '));
-    this.editMode = true;
-  }
-  addColumn() {
-    this.table.forEach(row => row.push(''));
+  edit(element: any) {
     this.editMode = true;
   }
 
-  edit() {
+  addRow() {
+      this.dataSource.data.push(this.dataSource.data[0].fill(''));
+      this.editMode = true;
+    }
+
+  deleteRow() {
+    this.dataSource.data.pop();
+    this.editMode = true;
+  }
+
+  addColumn() {
+    this.dataSource.data.forEach(row => row.push(''));
     this.editMode = true;
   }
 
   save() {
-    if (this.editMode) {
-      // Exit edit mode
-      this.editMode = false;
-      
-      // Save data to local storage
-      localStorage.setItem('table', JSON.stringify(this.table));
-      
-      // Convert table data to workbook
-      const worksheet = XLSX.utils.json_to_sheet(this.table);
-      const workbook = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
-
-      // Save workbook to Excel file
-      const file = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-
-      const blob = new Blob([file], { type: 'application/octet-stream' });
-      const url = URL.createObjectURL(blob);
-
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'table.xlsx';
-      
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-      URL.revokeObjectURL(url);
-
-    }
-  }
+    // Exit edit mode
+    this.editMode = false;
+    console.log(this.dataSource.data);
+    // Save data to local storage
+    localStorage.setItem('table', JSON.stringify(this.dataSource.data));
+    console.log(localStorage.getItem);
+    const locale = 'he'; // set the locale to Hebrew
   
+    // Convert table data to workbook
+    const date_format = {day: undefined, month: undefined, year: undefined, timeZone: 'UTC'};
+
+    // Convert table data to workbook
+    const worksheet = XLSX.utils.json_to_sheet(this.dataSource.data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+    
+      // Save workbook to Excel file
+    const file = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' ,cellDates: true});
+
+    const blob = new Blob([file], { type: 'application/octet-stream' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'table.xlsx';
+    
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+
+  }
+
+
+  formatDate(date: Date, format: string, locale: string) {
+    const options = { day: undefined, month: undefined, year: undefined, timeZone: 'UTC' };
+    const formatter = new Intl.DateTimeFormat(locale, options);
+    const parts = formatter.formatToParts(date);
+    const replacements: { [key: string]: string } = {};
+    parts.forEach(part => {
+    if (part.type === 'day') {
+    replacements['dd'] = part.value.padStart(2, '0');
+    replacements['d'] = part.value;
+    } else if (part.type === 'month') {
+    replacements['mm'] = part.value.padStart(2, '0');
+    replacements['m'] = part.value;
+    } else if (part.type === 'year') {
+    replacements['yyyy'] = part.value;
+    }
+    });
+    let result = format;
+    Object.keys(replacements).forEach(key => {
+    result = result.replace(key, replacements[key]);
+    });
+    return result;
+  }
+
+  update(element: any, index: number) {
+      element[index] = element.value;
+      // update the table array with the new value
+      const rowIndex = this.dataSource.data.indexOf(element);
+      this.dataSource.data[rowIndex][index] = element.value;
+      this.editMode = false;
+      localStorage.setItem('table', JSON.stringify(this.dataSource.data));
+  }
+
+
 }
